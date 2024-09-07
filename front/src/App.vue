4f849a2f7eda362d4e2c5a9fe5a989d778c97195
Aymen Alsuhaiqi <engaymen.work@gmail.com>
2024-09-07 17:37:45 +0300

    feature/add some animation in pages

--- front/src/App.vue
+++ front/src/App.vue
@@ -6,11 +6,26 @@ import Navbar from './components/navbar.vue'
 <template>
   <main class=" bg-white dark:bg-slate-900 h-screen">
     <Navbar/>
+    <transition name="app">
+      <RouterView/>
 
-    <RouterView/>
+    </transition>
   </main>
   
 </template>
 
 <style scoped>
+.app-enter-from{
+  opacity: 0;
+  /* transform: scale(0.5); */
+  /* transform: translateX(-100%); */
+}
+.app-enter-to{
+  opacity: 5;
+  /* transform: scale(1); */
+  /* transform: translateX(0); */
+}
+.app-enter-active{
+  transition:all 1.0s ease
+}
 </style>
--- front/src/views/NotFoundView.vue
+++ front/src/views/NotFoundView.vue
@@ -14,12 +14,12 @@ defineProps({
 
 <template>
     <section class="text-center flex flex-col justify-center items-center h-96">
-      <i class="pi pi-exclamation-triangle text-yellow-500 text-7xl mb-5"></i>
-      <h1 class="text-6xl font-bold mb-4">{{ error }}</h1>
-      <p class="text-xl mb-5">{{ msg }}</p>
+      <i class="pi pi-exclamation-triangle animate-bounce text-yellow-500 text-7xl mb-5"></i>
+      <h1 class="text-6xl font-bold dark:text-white mb-4">{{ error }}</h1>
+      <p class="text-xl mb-5 dark:text-white">{{ msg }}</p>
       <RouterLink
         to="/"
-        class="text-white bg-green-700 hover:bg-green-900 rounded-md px-3 py-2 mt-4"
+        class="text-white bg-green-700 animate-pulse hover:bg-green-900 rounded-md px-3 py-2 mt-4"
         >Go Back</RouterLink
       >
     </section>
